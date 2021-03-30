9eda73ffcffa119ff8ae92a571e2dea4abba09df
yerriswamyreddyb <yerriswamyreddyb@192.168.1.102>
2021-03-30 10:19:07 +0530

    Initial commit with spring-boot and aws lamda handler

---
+++ src/main/java/com/example/demo/DemoAWSHandler.java
@@ -0,0 +1,9 @@
+package com.example.demo;
+
+import org.springframework.cloud.function.adapter.aws.SpringBootRequestHandler;
+
+public class DemoAWSHandler extends SpringBootRequestHandler<String, Object>{
+	
+	
+
+}
---
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,21 @@
+package com.example.demo;
+
+import java.util.function.Supplier;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+	
+	@Bean
+	public Supplier<String> printHello() {
+		return () ->  "Hello Spring Boot with AWS";
+	}
+
+}
